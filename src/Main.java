1cbaa3fb1cfe375f4f2980ad13b4543c8c49a00d
sergf <sergfibih@ya.ru>
2024-11-01 10:46:06 +1000

    создал вторую ветку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 11;
+        if (age>=18){
+            System.out.println("если возраст человека равен " +age +", то он совершеннолетний");
+        } else {
+            System.out.println("если возраст человека равен " +age +", то он не достиг " +
+                    "совершеннолетия, надо подождать");
+        }
     }
 }
